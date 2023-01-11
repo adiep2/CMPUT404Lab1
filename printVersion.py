c8f2d2804cd30a6356d6f725c69832c530625b72
Aaron Diep <diep@ualberta.ca>
2023-01-11 12:21:57 -0700

    Added functionality to print its own code

--- printVersion.py
+++ printVersion.py
@@ -2,3 +2,4 @@ import requests as rq
 
 print(rq.__version__)
 print(rq.get('http://google.com/'))
+print(rq.get('https://raw.githubusercontent.com/adiep2/CMPUT404Lab1/main/printVersion.py').content)
