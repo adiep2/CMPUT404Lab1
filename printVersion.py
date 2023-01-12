878260eedc54a0b9e007c1564c51978445ffc6d4
Aaron Diep <diep@ualberta.ca>
2023-01-11 17:29:47 -0700

    Small modificaiton after visiting lab session

--- printVersion.py
+++ printVersion.py
@@ -1,5 +1,5 @@
 import requests as rq
 
 print(rq.__version__)
-print(rq.get('http://google.com/'))
-print(rq.get('https://raw.githubusercontent.com/adiep2/CMPUT404Lab1/main/printVersion.py').content)
+print(rq.get('http://google.com/').text)
+print(rq.get('https://raw.githubusercontent.com/adiep2/CMPUT404Lab1/main/printVersion.py').text)
